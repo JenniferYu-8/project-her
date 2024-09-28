9dd835c2da2740bef6d3aa45513caafdb0a30dd0
Homey Poon <pn.homey@gmail.com>
2024-09-28 15:51:00 -0400

    Updated community resources

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,22 @@ from flask import Flask
 # initalize flask app
 app = Flask(__name__)
 
-PREAMBLE = "You are a story-teller who is funny and engaging. You use a lot of imagery. You are taling to a student who is looking to do AI."
+name = request.json.get("name")
+interest = request.json.get('interest')
+gender = request.json.get("gender")
+
+PREAMBLE = f'''You are a story-teller who is funny and engaging. You use a lot of imagery. You are taling to a student who is looking to get into the field of {interest}.'''
 
 # Prompts
-COMMUNITY_RESOURCES = '''I am a student looking to get into AI, what resources do you recommend for me to connect with other gender minorities interested in the field?'''
+COMMUNITY_RESOURCES = f'''My name is {name}. I am a student who is super interested in {interest}. I am a {gender} so i am part of the gender minority, so what resources do you recommend for me to connect with other {gender} like me interested in the field?'''
 
 
 co = cohere.Client(api_key="HNuszEemO11A2nbVQL6He6hRujnGG1OcvYH87m2c",)
 
 
-@app.route('/communityResources')
+@app.route('/communityResources', methods=['POST'])
 def get_community_resources():
-
+    
     chat = co.chat(
         preamble=PREAMBLE,
         message=COMMUNITY_RESOURCES,
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 // import logo from './logo.svg'
-import './App.css'
-import MyBook from './components/MyBook.jsx'
+import "./App.css";
+import MyBook from "./components/MyBook.jsx";
 // import Page from './components/Page.jsx'
-import React from 'react';
+import React from "react";
 // import HTMLFlipBook from "react-pageflip";
 
 function App() {
   return (
-    <MyBook/>
-  )
+    <>
+      <h1>Community Resources</h1>
+      <MyBook />
+    </>
+  );
 }
 
 export default App;
